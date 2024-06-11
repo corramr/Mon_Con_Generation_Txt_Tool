5e66144ea1be73e62d219bcd8bb9b5984934e20a
cdevita <corrado.devita@txtgroup.com>
2024-06-11 14:52:27 +0200

    generate di.1.txt

--- src/generate_code.py
+++ src/generate_code.py
@@ -1,62 +1,141 @@
-def generate_code(processed_data):
+import os
 
-    generate_icd_types()
+# generate content for file "di.1.txt"
+def generate_di_1_code(file, device, device_data_dict):
+
+    # define text chunks class present "di.1.ada"
+    class TextChunks:
+        def __init__(self):
+            # Initializing each field as an empty list
+            self.monTypes = []
+            self.monGetProcedures = []
+
+    # create instance
+    text_chunks = TextChunks()
+
+    # loop over signals within device
+    for signal in device_data_dict["IN"].keys():
+        # loop over input messages within signal
+        for message in device_data_dict["IN"][signal]:
+            # store mon types
+            text_chunks.monTypes.append(
+                "type "
+                + device
+                + "_"
+                + message.variable_name
+                + "_"
+                + "Mon_Type   "
+                + "is limited private\n"
+            )
+
+            # store mon procedures
+            chunk = f"""procedure Get_{message.variable_name}
+   (Dev_Index : in  Fmsb_Config_Types.Vuhf_Index;
+   Mon       : in  {device}_{message.variable_name}_Mon_Type;
+   Value     : out {device}_Types.{message.variable_name}_Type;
+   Valid     : out Boolean);"""
+
+            text_chunks.monGetProcedures.append(chunk + "\n\n")
+
+    # write mon types
+    if len(text_chunks.monTypes) > 0:
+        # add heading comments
+        text_chunks.monTypes.insert(0, "-- Mon types\n")
+        # add some new empty lines at the end of mon types as separators
+        text_chunks.monTypes.append("\n\n\n")
+        # write
+        for mon_chunk in text_chunks.monTypes:
+            file.write(mon_chunk)
+
+    # write mon procedures
+    if len(text_chunks.monGetProcedures) > 0:
+        # add heading comments
+        text_chunks.monGetProcedures.insert(0, "-- Get mon procedures\n")
+        # write
+        for mon_chunk in text_chunks.monGetProcedures:
+            file.write(mon_chunk)
 
 
-def generate_icd_types():
+# generate content for file "di.2.txt"
+def generate_di_2_code(file, device, device_data_dict):
 
-    Format_Element_Array = [
-        "Frequency_Tens_Digit",
-        "Frequency_Ones_Digit",
-        "Frequency_Tenths_Digit",
-        "Frequency_Offset_50_Khz",
-        "Rt_Mode",
-    ]
+    # define text chunks class present "di.2.ada"
+    class TextChunks:
+        def __init__(self):
+            # Initializing each field as an empty list
+            self.tableDeclaration = {"in": [], "out": []}
+            self.messageDeclaration = {"in": [], "out": []}
+            self.tableAssignment = {"in": [], "out": []}
+            self.messageAssignment = {"in": [], "out": []}
+            self.monGetProcedures = []
 
-    Input_Output = "Input"
+    # create instance
+    text_chunks = TextChunks()
 
-    class Message:
-        def __init__(self, format_name, type, word, msb, lsb):
+    # # loop over signals within device
+    # for signal in device_data_dict.keys():
+    #     # loop over input messages within signal
+    #     for in_out in device_data_dict[signal].keys():
+    #         if in_out == "IN":
+    #             text_chunks.tableDeclaration["in"].append(
+    #                 "In_" + device + "_" + signal + "_Table"
+    #             )
+    #         elif in_out == "OUT":
+    #             text_chunks.tableDeclaration["out"].append(
+    #                 "Out_" + device + "_" + signal + "_Table"
+    #             )
 
-            self.format_name = format_name
-            self.type = type
-            self.word = word
-            self.msb = msb
-            self.lsb = lsb
+    # # write table declaration
+    # if len() > 0:
+    #     # add heading comments
+    #     text_chunks.tableDeclaration["in"].insert(0, "--Define input tables\n")
+    #     # write
 
-    # Creating a list of Signal objects
-    signal = [
-        Message(
-            format_name="Frequency_Tens_Digit", type="Scaled_Int", word=1, msb=0, lsb=3
-        ),
-        Message(
-            format_name="Frequency_Ones_Digit", type="Scaled_Int", word=2, msb=4, lsb=7
-        ),
-        Message(
-            format_name="Frequency_Tenths_Digit",
-            type="Scaled_Int",
-            word=3,
-            msb=8,
-            lsb=11,
-        ),
-        # Add more objects as needed
-    ]
 
-    var = "vuhf"
-    filename = f"{var}_icd_types.txt"
+# generate content for file "icd_types.1.txt"
+def generate_icd_types_1_code(file, device_data_dict):
+    pass
 
-    content = f"""
-           {signal[0].format_name} =>
-             (Kind           => Conversion_Types.Scaled_Int,
-              In_Validity    => (Kind => Conversion_Types.Msg_Only),
-              Scaled_Int_Fmt =>
-                (Location     => (Word => 0, Msb => 0, Lsb => 3),
-                 Msb_Sign_Bit    => False,
-                 Min_Value       => 0,
-                 Max_Value       => 150,
-                 Lsb_Weight      => 10)),
-    """
 
-    # Open the file in write mode and write the content
-    with open(filename, "w") as file:
-        file.write(content)
+# generate content for file "state.1.txt"
+def generate_state_1_code(file, device_data_dict):
+    pass
+
+
+# generate files
+def generate_code(data_dict):
+
+    # create output folder if not present
+    output_folder = "output"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for device, device_data_dict in data_dict.items():
+
+        # di.1.txt
+        file_name = device.lower() + "_di_1.txt"
+        file_path = os.path.join(output_folder, file_name)
+
+        with open(file_path, "w", encoding="utf-8") as di_1_file:
+            generate_di_1_code(di_1_file, device, device_data_dict)
+
+        # di.2.txt"
+        file_name = device.lower() + "_di_2_.txt"
+        file_path = os.path.join(output_folder, file_name)
+
+        with open(file_path, "w", encoding="utf-8") as di_2_file:
+            generate_di_2_code(di_2_file, device, device_data_dict)
+
+        # icd_types.1.txt
+        file_name = device.lower() + "_icd_types_1.txt"
+        file_path = os.path.join(output_folder, file_name)
+
+        with open(file_path, "w", encoding="utf-8") as icd_types_1_file:
+            generate_icd_types_1_code(icd_types_1_file, device_data_dict)
+
+        # state.1.txt
+        file_name = device.lower() + "_state_1.txt"
+        file_path = os.path.join(output_folder, file_name)
+
+        with open(file_path, "w", encoding="utf-8") as state_1_file:
+            generate_state_1_code(state_1_file, device_data_dict)
--- src/process_data.py
+++ src/process_data.py
@@ -1,142 +1,77 @@
+from collections import defaultdict
+
+
+# Define class: each row of the excel file will be an instance of this class
+class DataRow:
+    def __init__(
+        self,
+        device,
+        message_name,
+        variable_name,
+        word_start,
+        word_end,
+        msb,
+        lsb,
+        variable_type,
+        min,
+        max,
+        lsb_weight,
+        in_out,
+        mon_data_validity,
+        mon_format_validity,
+        index_name,
+        index_num,
+        enabled,
+    ):
+        self.device = device
+        self.message_name = message_name
+        self.variable_name = variable_name
+        self.word_start = word_start
+        self.word_end = word_end
+        self.msb = msb
+        self.lsb = lsb
+        self.variable_type = variable_type
+        self.min = min
+        self.max = max
+        self.lsb_weight = lsb_weight
+        self.in_out = in_out
+        self.mon_data_validity = mon_data_validity
+        self.mon_format_validity = mon_format_validity
+        self.index_name = index_name
+        self.index_num = index_num
+        self.enabled = enabled
+
+
 def process_data(raw_data):
 
-    # Initialized processed data
-    processed_data = []
+    # Initialize
+    data_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
-    # Get the 'function' column and remove duplicates
-    components = list(set(raw_data['Function']))
+    # Arrange data into a dictionary of  a list of objects
+    # The key is the device (VUHF, HF, EGI, MC ...)
+    # To each key is associated a number of objects (one object corresponds to one row in the excel file)
+    for _, row in raw_data.iterrows():
+        data_row = DataRow(
+            row["Function"],
+            row["Signal name"],
+            row["ICD VARIABLE_NAME"],
+            row["word start"],
+            row["word end"],
+            row["msb"],
+            row["lsb"],
+            row["type (IRS)"],
+            row["Min"],
+            row["Max"],
+            row["RES /\nLSB weight"],
+            row["IN / OUT"],
+            row["MON DATA VALIDITY"],
+            row["MON FORMAT VALIDITY"],
+            row["INDEX NAME"],
+            row["INDEX NUM"],
+            row["ENABLED"],
+        )
 
-    raw_data
+        data_dict[row["Function"]][row["IN / OUT"]][row["Signal name"]].append(data_row)
 
-    # for component in components:
-    #     component_dict = {}
-    #     for 
-    #     process_data.append()
-
-
-    processed_data = [
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Status_Data",
-            "Input_Output": "Input",
-            "Validity": False,
-            "Format_Name": "Frequency_Tens_Digit",
-            "Type": "Scaled_Int",
-            "Enum_Values": None,
-            "Record": True,
-            "Word": 1,
-            "Msb": 0,
-            "Lsb": 3,
-            "Msb_Sign_Bit": False,
-            "Lsb_Weight": 10,
-            "Max_Value": 150,
-            "Min_Value": 0,
-        },
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Status_Data",
-            "Input_Output": "Input",
-            "Validity": False,
-            "Format_Name": "Frequency_Ones_Digit",
-            "Type": "Scaled_Int",
-            "Enum_Values": None,
-            "Record": True,
-            "Word": 1,
-            "Msb": 4,
-            "Lsb": 7,
-            "Msb_Sign_Bit": False,
-            "Lsb_Weight": 1,
-            "Max_Value": 15,
-            "Min_Value": 0,
-        },
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Status_Data",
-            "Input_Output": "Input",
-            "Validity": False,
-            "Format_Name": "Frequency_Tenths_Digit",
-            "Type": "Scaled_Int",
-            "Enum_Values": None,
-            "Record": True,
-            "Word": 1,
-            "Msb": 8,
-            "Lsb": 11,
-            "Msb_Sign_Bit": False,
-            "Lsb_Weight": 1,
-            "Max_Value": 15,
-            "Min_Value": 0,
-        },
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Status_Data",
-            "Input_Output": "Input",
-            "Validity": False,
-            "Format_Name": "Frequency_Offset_50_Khz",
-            "Type": "Bool",
-            "Enum_Values": None,
-            "Record": False,
-            "Word": 1,
-            "Msb": 12,
-            "Lsb": 12,
-            "Msb_Sign_Bit": None,
-            "Lsb_Weight": None,
-            "Max_Value": None,
-            "Min_Value": None,
-        },
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Status_Data",
-            "Input_Output": "Input",
-            "Validity": False,
-            "Format_Name": "Rt_Mode",
-            "Type": "Lookup_Straight_2_Bit",
-            "Enum_Values": {
-                "Format": "Bin",
-                "Values": [
-                    {"Dec_Code": 0, "Value": "Tr"},
-                    {"Dec_Code": 1, "Value": "Tr_G"},
-                    {"Dec_Code": 2, "Value": "Spare"},
-                    {"Dec_Code": 3, "Value": "Unknown"},
-                ],
-            },
-            "Record": False,
-            "Word": 2,
-            "Msb": 0,
-            "Lsb": 1,
-            "Msb_Sign_Bit": None,
-            "Lsb_Weight": None,
-            "Max_Value": None,
-            "Min_Value": None,
-        },
-        {
-            "Generate_Code": "True",
-            "Device_Name": "Vuhf",
-            "Signal_Name": "Vux_Data_Fill",
-            "Input_Output": "Output",
-            "Validity": False,
-            "Format_Name": "Command_Word",
-            "Type": "Lookup_Straight_32_Val",
-            "Enum_Values": {
-                "Format": "Hex",
-                "Values": [
-                    {"Dec_Code": 10, "Value": "Wod_Mwod_Load"},
-                    {"Dec_Code": 12, "Value": "Have_Quick_Ii_Fmt_Load"},
-                    {"Dec_Code": 30, "Value": "Saturn_Special_Training_Frequencies"},
-                ],
-            },
-            "Record": False,
-            "Word": 1,
-            "Msb": 0,
-            "Lsb": 15,
-            "Msb_Sign_Bit": None,
-            "Lsb_Weight": None,
-            "Max_Value": None,
-            "Min_Value": None,
-        },
-    ]
-    return processed_data
+    # Return dictionary
+    return data_dict
