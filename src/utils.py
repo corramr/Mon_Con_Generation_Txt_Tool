1520431f86a2084e85c96f3ddb3f9ab47487132d
Ivan Bonmassar <ivan.bonmassar@txtgroup.com>
2025-01-13 15:22:50 +0100

    fix for special characters (/,-)

--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,7 @@
 
 #Filters out blank spaces at beginning and end and enables Pascal case 
 def clean_string(text):
-    result = '_'.join(result.capitalize() for result in text.split('_')).strip()
+    result = text.replace("/", "_").replace("-", "_")
+    result = '_'.join(result.capitalize() for result in result.split('_')).strip()
+    print(result)
     return result
