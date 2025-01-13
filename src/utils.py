8aec6e927b908456746c95ba55db80f000739aec
Ivan Bonmassar <ivan.bonmassar@txtgroup.com>
2025-01-13 15:09:57 +0100

    quick fix for pascal case

--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,5 @@
 
 #Filters out blank spaces at beginning and end and enables Pascal case 
 def clean_string(text):
-    result = (('_'.join(result.capitalize() for result in text.split('_'))).lower()).strip()
+    result = '_'.join(result.capitalize() for result in text.split('_')).strip()
     return result
